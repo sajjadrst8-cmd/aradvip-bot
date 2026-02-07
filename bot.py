0ca391ff22ec1cb452d39d026ec2d9223ab039f4
sajjadrst8-cmd <sajjad.rst8@gmail.com>
2026-02-07 11:05:24 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("8589323355:AAFCd6pR3VOLuoCUyId3Kz6hsMl5wZITagE")
+TOKEN = os.getenv("8531397872:AAHmyli0cKo2w_Pkg4X9x-JZzE-NXVGsaaE")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ ربات روی سرور آنلاین است")
