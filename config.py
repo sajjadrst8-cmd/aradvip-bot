a3949b732d98fb49d1dd08341ad20aa7f94df46a
sajjadrst8-cmd <sajjad.rst8@gmail.com>
2026-02-09 10:13:05 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+# ==============================
+# تنظیمات اصلی ربات و اتصال Marzban
+
+# توکن ربات تلگرام
+TELEGRAM_BOT_TOKEN = "8531397872:AAHQbLN-Frn1GfTboMYpol36LkepNak1r3M"
+
+# اطلاعات Marzban
+MARZBAN_USERNAME = "1804445169"
+MARZBAN_PASSWORD = "1804445169"
+MARZBAN_API_URL = "https://v2inj.galexystore.ir/api"
+
+# ادمین‌ها
+ADMINS = [863961919]  # آی‌دی تلگرام ادمین‌ها
