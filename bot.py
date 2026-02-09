96a5d1eb4bb7b2df9fd1d3bcc3182b1bdfd146cc
sajjadrst8-cmd <sajjad.rst8@gmail.com>
2026-02-09 09:05:31 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,15 @@
-print("BOT FILE STARTED")
-import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+# 🔴 توکن رو مستقیم اینجا بذار
+BOT_TOKEN = "8531397872:AAHQbLN-Frn1GfTboMYpol36LkepNak1r3M"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("ربات با موفقیت بالا اومد ✅")
+
 def main():
-    BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-
-    if not BOT_TOKEN:
-        raise RuntimeError("TELEGRAM_BOT_TOKEN is not set")
-
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-
-    async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text("Bot is running ✅")
-
     app.add_handler(CommandHandler("start", start))
-
     app.run_polling()
 
 if __name__ == "__main__":
