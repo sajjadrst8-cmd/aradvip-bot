111713dcae3702ce0d73204946b0a718c46cee6d
sajjadrst8-cmd <sajjad.rst8@gmail.com>
2026-02-09 08:40:18 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.getenv("8531397872:AAEREgKU9CxllZHn1DCnI-_1bPCkgWDf9Yk
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("ربات با موفقیت اجرا شد ✅")
