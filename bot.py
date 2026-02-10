fe79ca17b0067e2f09078a12a546a02feb8cea78
sajjadrst8-cmd <sajjad.rst8@gmail.com>
2026-02-10 16:31:18 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ import sqlite3
 import random
 import string
 from datetime import datetime
-from aiogram import Bot, Dispatcher, types, executor
+from aiogram import Bot, Dispatcher, types
+import asyncio
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
