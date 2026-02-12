e007b8f31c1eb439d4d5ed3c626c0cf5ebf78d7a
sajjadrst8-cmd <sajjad.rst8@gmail.com>
2026-02-12 12:54:12 +0330

    Update loader.py

--- loader.py
+++ loader.py
@@ -2,6 +2,9 @@ import os
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-bot = Bot(token=os.getenv("BOT_TOKEN"))
+# خوندن مقادیر از تنظیمات سیستمی Railway
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
-ADMIN_ID = 863961919
