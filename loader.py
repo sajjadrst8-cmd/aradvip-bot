491415ccd6d1f12103529cb28c192b025ccf1d3e
sajjadrst8-cmd <sajjad.rst8@gmail.com>
2026-02-12 10:57:32 +0330

    Create loader.py

---
+++ loader.py
@@ -0,0 +1,7 @@
+import os
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+bot = Bot(token=os.getenv("BOT_TOKEN"))
+dp = Dispatcher(bot, storage=MemoryStorage())
+ADMIN_ID = 863961919
