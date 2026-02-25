102af1218d877e132e6f2d97f3592a4110fbf4bc
sajjadrst8-cmd <sajjad.rst8@gmail.com>
2026-02-25 12:33:32 +0330

    Update loader.py

--- loader.py
+++ loader.py
@@ -2,9 +2,9 @@ import os
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-# خوندن مقادیر از تنظیمات سیستمی Railway
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
+storage = MemoryStorage() # ابتدا در یک متغیر تعریف کن
 bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot, storage=MemoryStorage())
+dp = Dispatcher(bot, storage=storage) # متغیر را اینجا پاس بده
