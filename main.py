46cd85af5317cac66a05e7e0e04ed597b68a3d4e
sajjadrst8-cmd <sajjad.rst8@gmail.com>
2026-02-25 12:06:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import logging
 from aiogram import executor
 from loader import dp
-import admin_handlers
-import marzban_handlers
-import buy_handlers
-import handlers
 
-
+# تنظیمات لاگینگ را ابتدا انجام بده
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
 
+# حالا هندلرها را ایمپورت کن (ترتیب مهم است)
+import admin_handlers
+import marzban_handlers
+import buy_handlers
+import handlers  # این همیشه آخری باشد چون هندلرهای عمومی در آن است
+
 async def on_startup(dispatcher):
     print("🚀 Bot is Online!")
     logger.info("ربات با موفقیت در ری‌ل‌وی اجرا شد.")
 
 if __name__ == '__main__':
-    # شروع به کار ربات
-    # skip_updates=True باعث می‌شود پیام‌هایی که موقع خاموش بودن ربات ارسال شده، نادیده گرفته شوند
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
